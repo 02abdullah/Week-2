a9bab8600b9992d2ab950878ce079dd7295cc299
02abdullah <32358266+02abdullah@users.noreply.github.com>
2017-10-19 17:48:50 +0100

    Add files via upload

---
+++ 5aSide.py
@@ -0,0 +1,10 @@
+#Question5
+print ('Number of teams: ' + str(172//5))
+print ('Number of pupils left out: '+ str(172%5))
+
+#Question6
+number_of_pupils = 172
+teams = number_of_pupils//5
+pupils_left_out = number_of_pupils%5
+print('The number of 5 a side teams are: ' + str(teams))
+print ('The number of pupils left out is: ' + str(pupils_left_out))
